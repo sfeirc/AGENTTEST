cd8fd0ae3cdc6ff8c78546644cd1a02a57983eec
Phase3D Campaign <campaign@test.local>
2026-07-17 12:29:14 +0000

    campaign: task s3-1784291211415972800-median

---
+++ stats_median_1784291211415972800.py
@@ -0,0 +1,8 @@
+def median(xs):
+    s = sorted(xs)
+    n = len(s)
+    if n == 0:
+        raise ValueError("median() arg is an empty sequence")
+    if n % 2 == 1:
+        return s[n // 2]
+    return (s[n // 2 - 1] + s[n // 2]) / 2
